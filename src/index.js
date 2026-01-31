e18a97f96d3fcd9ca1506b3b398dfe3a08ea3e19
Onkar Chougule <onkarchougule99@gmail.com>
2026-01-31 22:33:24 +0530

    add global err middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { Config } from "./config/config.js";
 import process from "process";
+import logger from "./config/logger.js";
 
 const app = express();
 
@@ -11,11 +12,28 @@ app.get("/", (req, res) => {
 const StartServer = () => {
   try {
     const port = Config.PORT || 1337;
-    app.listen(port, () => console.log("app is listening on port : ", port));
+    app.listen(port, () => {
+      logger.info(`server is listening on port : ${port}`);
+    });
   } catch (error) {
     console.error(error);
     process.exit(1);
   }
 };
-
 StartServer();
+
+app.use((err, req, res, next) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
+});
