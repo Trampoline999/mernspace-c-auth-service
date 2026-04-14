5ebb0ce2e9fa119c9a76507cad41bee3460600c3
Onkar Chougule <onkarchougule99@gmail.com>
2026-04-14 22:27:18 +0530

    test(app): remove unnecessary async from describe callback
    - Removed async keyword from the top-level describe block in app.spec.mjs
    - Corrected import statement case for UserService in auth.routes.js for consistency
    - Updated formatting to ensure clean and consistent code style in auth.routes.js

--- app.spec.mjs
+++ app.spec.mjs
@@ -2,7 +2,7 @@ import { describe, expect, test } from "@jest/globals";
 import request from "supertest";
 import app from "./src/app.js";
 
-describe("App", () => {
+describe("App", async () => {
   test("should give status code :200", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
